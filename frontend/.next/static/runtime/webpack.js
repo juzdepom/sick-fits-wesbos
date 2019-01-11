bfbfae9a585d962f78f2f4d1e2266d259e4e80cb
Julia Gao Miller <julia.gao.miller@gmail.com>
2019-01-12 06:09:07 +0700

    .next stuff

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2b9cd62ec23751993646";
+/******/ 	var hotCurrentHash = "bb76d3d5b983082972b0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
