30bd5bbea0a2fc23f159116de42b88d47a80dcad
Julia Gao Miller <julia.gao.miller@gmail.com>
2019-01-12 04:52:20 +0700

    ✅ 43. Optimistic Response && Cache Updates with Apollo (8min)

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -28,28 +28,31 @@ class RemoveFromCart extends React.Component {
         id: PropTypes.string.isRequired,
     };
     // This gets called as soon as we get a response back from the server after a mutation has been performed
-    // update = (cache, payload) => {
-    //     // 1. first read the cache
-    //     const data = cache.readQuery({ query: CURRENT_USER_QUERY });
-    //     // 2. remove that item from the cart
-    //     const cartItemId = payload.data.removeFromCart.id;
-    //     data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
-    //     // 3. write it back to the cache
-    //     cache.writeQuery({ query: CURRENT_USER_QUERY, data });
-    // };
+    //otherwise, when we delete an item, there would be a pause before the item is removed
+    update = (cache, payload) => {
+        // 1. first read the cache (this is the apollo cache)
+        const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+        // 2. remove that item from the cart
+        const cartItemId = payload.data.removeFromCart.id;
+        data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+        // 3. write it back to the cache
+        cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+    };
     render() {
         return (
         <Mutation
             mutation={REMOVE_FROM_CART_MUTATION}
             variables={{ id: this.props.id }}
             update={this.update}
-            // optimisticResponse={{
-            //     __typename: 'Mutation',
-            //     removeFromCart: {
-            //         __typename: 'CartItem',
-            //         id: this.props.id,
-            //     },
-            // }}
+            //when we delete an item, it's going to optimistically immediately reply with this information
+            //this also allows for the instant UI update
+            optimisticResponse={{
+                __typename: 'Mutation',
+                removeFromCart: {
+                    __typename: 'CartItem',
+                    id: this.props.id,
+                },
+            }}
         >
             {(removeFromCart, { loading, error }) => (
             <BigButton
